d71c6de66b407678f283d8877ab3b23859d9f2d8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-19 22:23:14 +0200

    added header copyright comments

--- src/blufo.dpr
+++ src/blufo.dpr
@@ -1,3 +1,35 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Blufo
+///
+/// Copyright 2016-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://blufo.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Blufo
+///
+/// ***************************************************************************
+/// File last update : 2024-09-19T21:30:56.000+02:00
+/// Signature : 6a919bc315c7c701be54280f9226adaa55438036
+/// ***************************************************************************
+/// </summary>
+
 program blufo;
 
 uses
--- src/fMain.pas
+++ src/fMain.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Blufo
+///
+/// Copyright 2016-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://blufo.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Blufo
+///
+/// ***************************************************************************
+/// File last update : 2024-09-19T21:37:12.000+02:00
+/// Signature : 604e69bc697dcc5d753cac016274a9f5ba511a97
+/// ***************************************************************************
+/// </summary>
+
 unit fMain;
 
 interface
